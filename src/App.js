fe890b07e7f1a3d1505bc0532b0edc57c17a092a
Harshith Akkapelli <harshith.akkapelli@sjsu.edu>
2025-02-23 19:18:28 -0800

    Kairos - Frontend template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage"; // Create this page
+import SearchPage from "./pages/SearchPage"; // Create this page
+import NotificationsPage from "./pages/NotificationsPage"; // Create this page
+import AuthProviderComponent from "./components/AuthProvider";
+import Layout from "./Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProviderComponent>
+      <AuthProvider>
+        <Router>
+          <div className="flex flex-col min-h-screen">
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/home" element={<Layout />}>
+                <Route index element={<HomePage />} />
+              </Route>
+              <Route path="/search" element={<Layout />}>
+                <Route index element={<SearchPage />} />
+              </Route>
+              <Route path="/notifications" element={<Layout />}>
+                <Route index element={<NotificationsPage />} />
+              </Route>
+              <Route path="/profile" element={<Layout />}>
+                <Route index element={<ProfilePage />} />
+              </Route>
+            </Routes>
+          </div>
+        </Router>
+      </AuthProvider>
+    </AuthProviderComponent>
   );
 }
 
